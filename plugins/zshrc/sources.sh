5aa9c333122d94e05da5b60b5f77ba34007659a9
Fritz Duchardt <fritz@duchardt.net>
2024-01-12 10:45:47 +0100

    Corrected sources

--- plugins/zshrc/sources.sh
+++ plugins/zshrc/sources.sh
@@ -1,4 +1,1 @@
 source ~/projects/github/k8s-tools/config_rc.sh
-source ~/projects/github/k8s-tools/config_rc.sh
-source ~/projects/private/scripts/tax.sh
-source ~/projects/private/scripts/aliases.sh
