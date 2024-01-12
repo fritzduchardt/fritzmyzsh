1b0cd13c2e2defb01ff95ca4dfc1f7eb94888c91
Fritz Duchardt <fritz@duchardt.net>
2024-01-12 09:33:28 +0100

    Corrected paths

--- plugins/zshrc/sources.sh
+++ plugins/zshrc/sources.sh
@@ -1,4 +1,4 @@
-source ~/projects/private/k8s-tools/config_rc.sh
-source ~/projects/private/k8s-tools/config_rc.sh
+source ~/projects/github/k8s-tools/config_rc.sh
+source ~/projects/github/k8s-tools/config_rc.sh
 source ~/projects/private/scripts/tax.sh
 source ~/projects/private/scripts/aliases.sh
