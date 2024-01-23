d186b3a39c12b50e1fda072154586d4b9e0f19da
Fritz Duchardt <fritz@duchardt.net>
2024-01-23 18:32:12 +0100

    Improve fzf default command

--- plugins/zshrc/env.sh
+++ plugins/zshrc/env.sh
@@ -7,4 +7,4 @@ export GOROOT=/usr/local/go
 export MCFLY_DELETE_WITHOUT_CONFIRM=true
 
 # FZF
-export FZF_DEFAULT_COMMAND=fd --type f --hidden --exclude .git
+export FZF_DEFAULT_COMMAND="fd --type f --hidden --exclude .git"
