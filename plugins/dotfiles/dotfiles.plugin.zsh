4e8547f6c91d92886fb94af25a51ef10fa204dfa
Fritz Duchardt <fritz@duchardt.net>
2024-01-23 14:19:31 +0100

    Added vimrc

--- plugins/dotfiles/dotfiles.plugin.zsh
+++ plugins/dotfiles/dotfiles.plugin.zsh
@@ -1,9 +1,15 @@
-dotfile_location=~/.config
+dotfile_config_location=~/.config
+dotfile_location=~/
 for file in $ZSH_CUSTOM/lib/*.sh; do
     source $file
 done
 
+# Copy dot files
+for file in $ZSH_CUSTOM/plugins/dotfiles/.*rc; do
+    lib::exec cp -f "$file" "$dotfile_location"
+done
+
+# Copy config files to home directory
 for dir in $ZSH_CUSTOM/plugins/dotfiles/files/*; do
-    dest="$dotfile_location"
-    lib::exec cp -ur "$dir" "$dest"
+    lib::exec cp -ur "$dir" "$dotfile_config_location"
 done
