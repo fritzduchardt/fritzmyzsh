0d6b4ad4c822bd05c3b06fdca38b02b5269276f2
Fritz Duchardt <fritz@duchardt.net>
2024-01-27 11:55:11 +0100

    Configure mcfly

--- plugins/zshrc/env.sh
+++ plugins/zshrc/env.sh
@@ -9,3 +9,5 @@ export MCFLY_DELETE_WITHOUT_CONFIRM=true
 # FZF
 export FZF_DEFAULT_COMMAND="fd --type f --hidden --exclude .git"
 export MCFLY_FUZZY=2
+export MCFLY_RESULTS=20
+export MCFLY_PROMPT="?"
