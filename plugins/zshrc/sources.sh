130c9018f73c0913645f3d6e49145e626b5a047b
Fritz Duchardt <fritz@duchardt.net>
2025-04-25 18:25:48 +0200

    chore: Add ai-tools configuration to shell sources shell sources shell sources shell sources

--- plugins/zshrc/sources.sh
+++ plugins/zshrc/sources.sh
@@ -3,3 +3,4 @@ source /home/fritz/projects/github/k8s-tools/lib/utils.sh
 source /home/fritz/projects/github/k8s-tools/configrc.sh
 source /home/fritz/projects/github/k8s-admin-shell/configrc.sh
 source /home/fritz/projects/github/linux-tools/configrc.sh
+source /home/fritz/projects/github/ai-tools/configrc.sh
